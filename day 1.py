951157e0233e04fbd27c8ed4171b518104d223f4
Dnyanesh Warade <warade.dnyanesh@gmail.com>
2022-09-22 12:51:46 +0530

    first commit

---
+++ day 1.py
@@ -0,0 +1,198 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+rent = 20000
+petrol = 1000
+groceries = 7000
+total = rent+petrol+groceries
+print total
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print(total)
+28000
+gabbar = "amjad khan"
+jay = "Amitabh"
+thakur = "Sanjeev"
+type(gabbar)
+<class 'str'>
+name = gabbar+jay+thakur
+print
+<built-in function print>
+
+print (name)
+amjad khanAmitabhSanjeev
+learn python = true
+SyntaxError: invalid syntax
+learn_python = false
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    learn_python = false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+ty
+learn_python=False
+learn_python=True
+type (learn_python)
+<class 'bool'>
+True = 7
+SyntaxError: cannot assign to True
+_test = 8
+print(_test)
+8
+0khjhj=True
+SyntaxError: invalid decimal literal
+bade_bhai = 1
+bade_Bhai=2
+print(bade_Bhai)
+2
+0khjhj=True
+SyntaxError: invalid decimal literal
+variable = 6
+variable
+6
+variable="gulab jamoon"
+variable
+'gulab jamoon'
+id(6)
+2329786384784
+id(variable)
+2329823415344
+variable1 =varible
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    variable1 =varible
+NameError: name 'varible' is not defined. Did you mean: 'variable'?
+variable1= variable
+id(variable1)
+2329823415344
+id(variable)
+2329823415344
+variable
+'gulab jamoon'
+variable1
+'gulab jamoon'
+variable1="kachori"
+variable
+'gulab jamoon'
+break = 5
+SyntaxError: invalid syntax
+birth_year=1989
+cuu_year=2022
+print(age = cuu_year-birth_year)
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    print(age = cuu_year-birth_year)
+TypeError: 'age' is an invalid keyword argument for print()
+age=cuu_year-birth_year
+age
+33
+name = "kshama"
+middle_name="Dnyanesh"
+last_name="Warade"
+full_name=name+middle_name+last_name
+full_name
+'kshamaDnyaneshWarade'
+name="Kshama/t"
+full_name
+'kshamaDnyaneshWarade'
+name="Kshama "
+full_name
+'kshamaDnyaneshWarade'
+_nation 1record=1
+SyntaxError: invalid decimal literal
+record1 record=2
+SyntaxError: invalid syntax
+_namtion=1
+1record=2
+SyntaxError: invalid decimal literal
+record1=2
+record_one=1
+record-one =3
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+
+record-one=1
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+record^pne=2
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+name = "+kshama+"
+full_name
+'kshamaDnyaneshWarade'
+print(name+" "+middle_name+" "+last_name)
++kshama+ Dnyanesh Warade
+print(name+" "+middle_name+" "+last_name)
++kshama+ Dnyanesh Warade
+
+base=10
+height=15
+triangle_area=1/2*base*height
+triangle_area
+75.0
+base=10.2
+height=20.5
+triangle_area=1/2*base*height
+triangle_area
+104.55
+round(triangle_area,1)
+104.5
+3**
+SyntaxError: invalid syntax
+3**2
+9
+10+2*3
+16
+(10+2)*3
+36
+type(triangle_area)
+<class 'float'>
+complex=2+2i
+SyntaxError: invalid decimal literal
+
+cmp=2+2i
+SyntaxError: invalid decimal literal
+cl=2+3j
+type cl
+SyntaxError: invalid syntax
+type(cl)
+<class 'complex'>
+c2=3+6j
+c3=c2-cl
+c3
+(1+3j)
+format(6,'b')
+'110'
+//football
+SyntaxError: invalid syntax
+length_football=92
+width_football=48.8
+area_football=length_football*width_football
+area_football
+4489.599999999999
+potato_chips = 9
+packet_cost=1.49
+total_cost=potato_chips*packet_cost
+total_cost
+13.41
+return_amount=20-total_cost
+return_amount
+6.59
+area = 5.5**2
+area
+30.25
+cost = area*500
+cost
+15125.0
+format(17,b)
+Traceback (most recent call last):
+  File "<pyshell#110>", line 1, in <module>
+    format(17,b)
+NameError: name 'b' is not defined
+format(17,'b')
+'10001'
+drinks="coconut water"
+drinks[2]
+'c'
+drinks[7]
+' '
+drinks[-1]
+'r'
+drinks[0:3]
+'coc'
+drinks[2:6]
+'conu'
